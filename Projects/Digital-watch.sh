e342f320eaa9427d468a7177d997114c15eeed15
Sajid Shaikh <sajid0100shaikh@gmail.com>
2025-11-09 01:01:54 +0530

    Create Digital-watch.sh

---
+++ Projects/Digital-watch.sh
@@ -0,0 +1,12 @@
+root@SS:~# cat digiwatch.sh
+#!/bin/bash
+Red=$'\e[1;31m'
+Green=$'\e[1;32m'
+Blue=$'\e[1;34'
+
+while true
+do
+        clear
+        echo $Red $(date +%T)
+        sleep 1s
+done
