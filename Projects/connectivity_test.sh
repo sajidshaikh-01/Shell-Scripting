9ee35d85add124c4ab742433cc9970cf25f8b3eb
Sajid Shaikh <sajid0100shaikh@gmail.com>
2025-11-09 23:42:43 +0530

    Update connectivity_test.sh

--- Projects/connectivity_test.sh
+++ Projects/connectivity_test.sh
@@ -3,7 +3,7 @@ ping -c 1 $site
 
 if [[ $? -eq 0 ]]
 then
-        echo "you are sucessfully connected to $site"
+        echo "you are sucessfully connected to this$site"
 else
-        echo "you are disconnected to $site"
+        echo "you are disconnected to this$site"
 fi
