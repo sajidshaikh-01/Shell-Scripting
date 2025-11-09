20eac5eba7049b30a83145b1e36bda709d97acdc
Sajid Shaikh <sajid0100shaikh@gmail.com>
2025-11-09 23:43:59 +0530

    Update monitoring-free-disk-space.sh

--- Projects/monitoring-free-disk-space.sh
+++ Projects/monitoring-free-disk-space.sh
@@ -6,7 +6,7 @@
 
 FU=$(df -H | grep rootfs | awk '{print $5}' | tr -d %)
 TO="sajid0100shaikh@gmail.com"
-if [[ $FU -gt 80 ]]
+if [[ $FU -gt 90 ]]
 then
         echo "WARNING Disk Space is Low - $FU %" | mail -s "Disk Space Alert!!!!!!" $TO
 else
